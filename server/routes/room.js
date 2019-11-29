ad43075b1e6272b5e16b265f96bdc44a1074827c
EC2 Default User <ec2-user@ip-172-31-43-101.us-east-2.compute.internal>
2019-11-29 01:27:07 +0000

    server handles skip user turn

--- server/routes/room.js
+++ server/routes/room.js
@@ -10,5 +10,6 @@ router.post("/playCard", roomCon.playCard);
 router.post("/playerCards", roomCon.getPlayerCards);
 router.post("/askCard", roomCon.getCard);
 router.post("/boardCard", roomCon.getPlayCard);
+router.post("/passTurn", roomCon.passTurn);
 
 module.exports = router;
