c1b3defdb0c763ff65a950eb843469219a6eeb70
EC2 Default User <ec2-user@ip-172-31-23-191.us-east-2.compute.internal>
2019-11-29 02:16:04 +0000

    C

--- app/src/components/Game.js
+++ app/src/components/Game.js
@@ -8,4 +8,34 @@ class Game extends Component {
             cards: []
         }
     }
+
+    render() {
+        return (
+            <div className="card">
+            <form className="card-body" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <input
+                  type="number"
+                  name="room"
+                  className="form-control"
+                  placeholder="Room"
+                  //onChange={this.handleInput}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="number"
+                  name="player"
+                  className="form-control"
+                  placeholder="Player"
+                  //onChange={this.handleInput}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Go!
+              </button>
+            </form>
+        </div>
+        );
+    };
 }
